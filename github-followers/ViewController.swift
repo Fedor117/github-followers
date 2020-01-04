5e2aba63c61c02151424e0f73eeab0dce224ef21
Theodor Valiavko <fedor.valiavko@gmail.com>
2020-01-04 15:48:35 +0300

    Initial Commit

---
+++ github-followers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  github-followers
+//
+//  Created by Theodor Valiavko on 04/01/2020.
+//  Copyright © 2020 Fedor Valiavko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
