4772206aa05630d293777aeb87962e9fc32bb2ab
Theodor Valiavko <fedor.valiavko@gmail.com>
2020-01-06 20:22:27 +0200

    Project structure

---
+++ github-followers/Custom Views/GFAlertContainerView.swift
@@ -0,0 +1,30 @@
+//
+//  GFAlertContainerView.swift
+//  github-followers
+//
+//  Created by Theodor Valiavko on 06/01/2020.
+//  Copyright © 2020 Theodor Valiavko. All rights reserved.
+//
+
+import UIKit
+
+class GFAlertContainerView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
